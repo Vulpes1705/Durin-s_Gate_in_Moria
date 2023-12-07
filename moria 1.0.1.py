eea4f925e0fb358b7a19b312b9c666c8e2f5bda1
Vulpes1705 <109859387+Vulpes1705@users.noreply.github.com>
2023-12-07 14:13:28 +0300

    re-upload
    added new main file

---
+++ moria 1.0.1.py
@@ -0,0 +1,11 @@
+pass_phrase = ""
+print("Greetings, stranger. You now stand at the Doors of Durin. Speak Friend and Enter. ")
+pass_phrase = input()
+
+while pass_phrase != "Mellon":
+     print("Nothing happens. The gate remain stationary.")
+     print("Speak Friend and Enter. ")
+     pass_phrase = input()
+    
+print("The sequence is complete and the gates are open. You can enter Moria now.")
+    
